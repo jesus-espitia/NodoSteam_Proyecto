5cfc890c7ae8bc9cc25983c6e42968e58228dddc
jesus-espitia <espitiaguzmanjesusmanuel@gmail.com>
2024-07-27 00:32:12 -0500

    Primer Update

---
+++ templates/js/Script.js
@@ -0,0 +1,15 @@
+
+let matematicas = document.getElementById('matematicass');
+let quimica = document.getElementById('quimicaa');
+let fisica = document.getElementById('fisicaa');
+let tecnologia = document.getElementById('tecnologiaa');
+
+matematicas-addEventListener("click", function() {
+
+    Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Something went wrong!",
+        footer: '<a href="#">Why do I have this issue?</a>'
+    });
+})
